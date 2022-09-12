bd17770663347ce6c1305eea889afc3f15bc5077
Danny-Yin <95953989+Danny-Yin@users.noreply.github.com>
2022-09-11 23:05:41 -0700

    second2

--- app/src/main/java/edu/northeastern/NUMAD22Fa_jiajieyin/MainActivity.java
+++ app/src/main/java/edu/northeastern/NUMAD22Fa_jiajieyin/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
+    public void jiajieyin(View view){
+        Toast.makeText(getApplicationContext(),"jiajie-yin Email:dannyyin45@gmail.com",Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
